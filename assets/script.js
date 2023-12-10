323260a46fd9449e5d783925a8ef3fd51a45fe2c
Maximilian93B <147316673+Maximilian93B@users.noreply.github.com>
2023-12-09 22:01:21 -0400

    added button functions/handle form submission/renderResults func

--- assets/script.js
+++ assets/script.js
@@ -1,22 +1,23 @@
-// Add all API Scripts to HTML file
+// Add all API Scripts 
 
 // add DOM event listener to let html load first so we have no loading issues. 
 document.addEventListener("DOMContentLoaded", function() { 
 
     
-});
+
 // Declare all variables (const, let , var) --> We can Declare API url and Key so we dont need to C&P every time. 
  
 const startButton = document.getElementById("startButton")
-const inputForm = document.getElementById("inputForm")
+const inputForm = document.getElementById("activity-form")
 const locationInput = document.getElementById("locationInput")
 const submitButton = document.getElementById("submitButton")
 const googleMaps = document.getElementById("googleMaps") 
 const resultsContainer = document.getElementById("resultsContainer")
+const activityForm = document.getElementById("activity-form");
 
 
-const apiURL = apiURL 
-const apiKey = apiKey
+const apiURL = " add apiUrl" 
+const apiKey = " add apiKey"
 
 
 
@@ -24,6 +25,129 @@ const apiKey = apiKey
 startButton.addEventListener("click", function () {
     console.log("Get Started Button Clicked");
     inputForm.scrollIntoView({ behavior: "smooth" });
+});
+
+// Event listner to handle form
+submitButton.addEventListener("click", function (event ) {
+    event.preventDefault();
+    console.log("Submit Button Clicked")
+    
+    
+    // Fetch data from API using locationInput and selectedActivities
+   
+    const location = document.getElementById("locationInput").value;
+    const selectedActivities = getSelectedActivities();
+   
+    console.log("Selected Activities:",getSelectedActivities() );
+    console.log ("Form Submitted with location:",location);
+    
+   
+    //fetchGoogleMapsData(location,selectedActivites);
+
+});
+
+// function to put selected activites from checkboxes into array and declare in variable
+function getSelectedActivities() { 
+    const checkboxes = document.querySelectorAll('input[name="favoriteActivity"]:checked');
+    const selectedActivites = Array.from(checkboxes).map(checkbox => checkbox.value);
+    return selectedActivites; 
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function to render results from Google APIs into results containers
+function renderResults(places) {
+    resultsContainer.innerHTML = ""; 
+   
+    //loop for places to render each one 
+    places.forEach(element => {
+        const resultElement = document.createElement("div");
+        resultElement.classList.add("col-md-4") // bootstrap column class 
+
+        const resultContent = `
+    <div class="result">
+        <h3>${element.name}</h3>
+        <p>${element.address}</p>
+    </div>
+`;
+
+        resultElement.innerHTML = resultContent
+        resultsContainer.appendChild(resultElement);
+        
+    });
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+});
+/*
 
     //Handle Form submission --> add Event to "Submit Button" --> API form Data and then triggger Google Maps API
 
